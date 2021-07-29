453b1f85a0e244ad96b8574ffd2c818aa5225917
Pedro Henrique Gomes Venturott <pedro.venturott@gmail.com>
2021-07-29 09:10:51 -0300

    - Reorganized components and pipelines structure.

--- converting_pipeline/make_pipeline.py
+++ converting_pipeline/make_pipeline.py
@@ -2,13 +2,13 @@ import kfp
 import kfp.components as comp
 from kfp import dsl
 
-downloader_op = comp.load_component_from_file('./download-and-extract-google-drive-op/component.yaml')
-convert_tiny_darknet_2_tf_op = comp.load_component_from_file('./convert-tiny-darknet-2-tf-op/component.yaml')
+downloader_op = comp.load_component_from_file('./download_and_extract_google_drive_op.yaml')
+convert_tiny_darknet_2_tf_op = comp.load_component_from_file('./convert_tiny_darknet_2_tf_op.yaml')
 
 # prints contents of output folder form last component
-check_pipeline_output_op = comp.load_component_from_file('./check-pipeline-output-op/component.yaml')
+check_pipeline_output_op = comp.load_component_from_file('./check_pipeline_output_op.yaml')
 
-@dsl.pipeline(name='convert-tiny-darknet-2-tf-op')
+@dsl.pipeline(name='convert-tiny-darknet-2-tf-pl')
 def test_pipeline(google_drive_id, score_threshold):
 
     download_task = downloader_op(google_drive_id=google_drive_id)
--- training_pipeline/make_pipeline.py
+++ training_pipeline/make_pipeline.py
@@ -2,14 +2,14 @@ import kfp
 import kfp.components as comp
 from kfp import dsl
 
-downloader_op = comp.load_component_from_file('./download-and-extract-google-drive-op/component.yaml')
-merge_files_op = comp.load_component_from_file('./merge-files-op/component.yaml')
-convert_pascal_2_yolo_op = comp.load_component_from_file('./convert-pascal-2-yolo-op/component.yaml')
+downloader_op = comp.load_component_from_file('./download_and_extract_google_drive_op.yaml')
+merge_files_op = comp.load_component_from_file('./merge_files_op.yaml')
+convert_pascal_2_yolo_op = comp.load_component_from_file('./convert_pascal_2_yolo_op.yaml')
 
 # prints contents of output folder form last component
-check_pipeline_output_op = comp.load_component_from_file('./check-pipeline-output-op/component.yaml')
+check_pipeline_output_op = comp.load_component_from_file('./check_pipeline_output_op.yaml')
 
-@dsl.pipeline(name='test_1')
+@dsl.pipeline(name='training_pipeline')
 def test_pipeline(google_drive_id, classes):
 
     download_task = downloader_op(google_drive_id=google_drive_id)
